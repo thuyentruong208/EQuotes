c4ebcc5ae21faae6e133efda0ae55b8514f2fa6a
Thuyen Truong <thuyentruong.work@gmail.com>
2022-08-29 19:37:18 +0700

    add project

---
+++ Shared/EQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EQuotesApp.swift
+//  Shared
+//
+//  Created by Thuyên Trương on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct EQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
