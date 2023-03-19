adb0debdb721b6692363b99dab026b51f496d287
Thuyen Truong <thuyentruong.work@gmail.com>
2023-03-19 13:22:15 +0700

    [ui] quick fix UI on mobile (dirty fix)

--- Shared/Modules/EQuoteView/AddNewQuoteView.swift
+++ Shared/Modules/EQuoteView/AddNewQuoteView.swift
@@ -11,6 +11,7 @@ struct AddNewQuoteView: View {
 
     @Environment(\.injected) private var injected: DIContainer
     @Environment(\.presentationMode) var presentationMode
+    @Environment(\.editMode) private var editMode
 
     @StateObject var newQuoteContent = NewQuoteContent()
     @State var addQuoteSuccess: Bool = false
@@ -21,75 +22,78 @@ struct AddNewQuoteView: View {
 
     var body: some View {
         Screen(colorTheme.secondaryBackground) {
-            VStack(alignment: .leading) {
+            ScrollView {
+                VStack(alignment: .leading) {
 
-                HStack {
-                    QuoteItemRow(newQuoteContent.toQuoteItem(),
-                                 show: .constant(true))
-                    .border(Color.gray, width: 1)
-                    .cornerRadius(10)
-                    .frame(minWidth: 0, maxWidth: .infinity)
-
-
-                    Spacer(minLength: 30)
-
-                    BackQuoteItemRow(quoteItem: newQuoteContent.toQuoteItem(), show: .constant(true))
+                    HStack {
+                        QuoteItemRow(newQuoteContent.toQuoteItem(),
+                                     show: .constant(true))
                         .border(Color.gray, width: 1)
                         .cornerRadius(10)
                         .frame(minWidth: 0, maxWidth: .infinity)
+
+
+                        Spacer(minLength: 30)
+
+                        BackQuoteItemRow(quoteItem: newQuoteContent.toQuoteItem(), show: .constant(true))
+                            .border(Color.gray, width: 1)
+                            .cornerRadius(10)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                    }
+
+                    HStack {
+                        textField($newQuoteContent.content)
+                        Spacer(minLength: 30)
+
+                        VStack {
+                            textField($newQuoteContent.askContent)
+                            textField($newQuoteContent.askImages)
+                        }
+                    }
+
+                    Divider()
+                        .frame(height: 3)
+                        .background( Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
+                        .padding(.top, 30)
+
+                    HStack(spacing: 30) {
+#if os(macOS)
+                        if addQuoteSuccess {
+                            Image(systemName: "icloud.and.arrow.up.fill")
+                                .resizable()
+                                .foregroundColor(Color.green)
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+
+                        }
+                        Spacer(minLength: 250)
+#else
+#endif
+
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Text("END")
+                                .bold()
+                                .foregroundColor(Color.red)
+                        }
+                        .buttonStyle(RoundedRectangleButtonStyle(backgroundColor: Color.white))
+
+                        Button {
+                            submitNewQuote()
+
+                        } label: {
+                            Text("SUBMIT")
+                        }
+                        .buttonStyle(RoundedRectangleButtonStyle(backgroundColor: Color.white))
+                    }
+                    .padding(.top, 20)
+
                 }
-
-                HStack {
-                    textField($newQuoteContent.content)
-                    Spacer(minLength: 30)
-
-                    VStack {
-                        textField($newQuoteContent.askContent)
-                        textField($newQuoteContent.askImages)
-                    }
-                }
-
-                Divider()
-                    .frame(height: 3)
-                    .background( Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
-                    .padding(.top, 30)
-
-                HStack(spacing: 30) {
-
-                    if addQuoteSuccess {
-                        Image(systemName: "icloud.and.arrow.up.fill")
-                            .resizable()
-                            .foregroundColor(Color.green)
-                            .scaledToFit()
-                            .frame(width: 20, height: 20)
-
-                    }
-
-                    Spacer(minLength: 250)
-
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Text("END")
-                            .bold()
-                            .foregroundColor(Color.red)
-                    }
-                    .buttonStyle(RoundedRectangleButtonStyle(backgroundColor: Color.white))
-
-                    Button {
-                        submitNewQuote()
-
-                    } label: {
-                        Text("SUBMIT")
-                    }
-                    .buttonStyle(RoundedRectangleButtonStyle(backgroundColor: Color.white))
-                }
-                .padding(.top, 20)
-
+                .padding(10)
             }
-            .padding(50)
+            .frame(width: UIScreen.main.bounds.width)
         }
-        .frame(width: 700)
 
     }
 }
